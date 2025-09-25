597c882fa6b96b0d3d0a5ab830fc3a8bbe25d019
Jason Immanuel <jasonimmanuel100@gmail.com>
2025-09-25 18:30:39 +0100

    Added new Leetcode Question

--- 169. Majority Element.py
+++ 169. Majority Element.py
@@ -11,8 +11,26 @@ Example 1:
 
 Input: nums = [3,2,3]
 Output: 3
+
 Example 2:
 
 Input: nums = [2,2,1,1,1,2,2]
 Output: 2
 """
+
+
+class Solution:
+    def majorityElement(self, nums: list[int]) -> int:
+        numbers_dict = {}
+
+        for number in nums:
+            if number in numbers_dict:
+                numbers_dict[number] += 1
+            else:
+                numbers_dict[number] = 1
+
+        return max(numbers_dict, key=numbers_dict.get)
+
+
+solution = Solution()
+print(solution.majorityElement([3,2,3,45]))
